2742260cf2356a8203452d94e46d05c0d396a21c
DeveroCode <charli.rey.misterio@gmail.com>
2023-07-23 20:30:41 -0600

    Udate: main finished

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import Header from './components/Header.vue';
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <Header />
+  <div class="max-w-full">
+    <Header />
+    <div class="max-w-7xl mx-auto">
+      <Main />
+    </div>
+  </div>
 </template>
 
 <style></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,76 @@
+<template>
+    <main class="grid grid-cols-12  mt-10 space-x-16">
+        <section class="col-span-6">
+            <!-- Firt content -->
+            <div class="mt-10">
+                <h1 class="text-6xl text-gray-700 font-bold leading-tight">Build anything. Reimagine commerce</h1>
+                <p class="text-gray-500 mt-5 mb-5">
+                    Millions of merchants trust Shopify to run their business--but they need developers to build the tools
+                    that
+                    help them achieve independence. Acces powerful APIs to bring your ideas to life on the platform that
+                    makes commerce better for everyone.
+                </p>
+
+                <!-- Other texts -->
+                <div>
+                    <h2 class="text-gray-700 text-xl mt-2 font-semibold">Develop apps that solve complex merchant problems
+                    </h2>
+                    <p class="text-gray-500 mt-5 mb-2">
+                        Expansive GraphQL and REST APIs let you integrate into Shopify[s admin, online store, cheackout, and
+                        more.
+                    </p>
+
+                    <h2 class="text-gray-700 text-xl mt-2 font-semibold">Integrate seamlessly into existing workflows</h2>
+                    <p class="text-gray-500 mt-5 mb-2">Embed your features with App Bridge. Create high quality experience
+                        with
+                        the
+                        ready-to-build Polaris
+                        component library.</p>
+
+                    <h2 class="text-gray-700 text-xl mt-2 font-semibold">Help merchants express themselves</h2>
+                    <p class="text-gray-500 mt-5 mb-2">Use Liquid to build templates. Get started with Dawn, our blazingly
+                        first,
+                        accessible reference theme
+                    </p>
+                </div>
+            </div>
+
+            <!-- Second content -->
+            <div class="mt-10">
+                <h2 class="text-gray-700 text-xl mt-2 font-semibold">Develop apps that solve complex merchant problems
+                </h2>
+
+                <div class="grid grid-cols-12 gap-4 mt-5">
+                    <!-- Tarjetas de arriba -->
+                    <div
+                        class="border border-gray-200 rounded-xl items-center md:col-span-6  flex space-x-3 px-2 py-2 shadow-md">
+                        <img src="../../public/icons/minero.png" alt="minero icon" class="w-16">
+                        <a href="#" class="text-xl font-semibold  text-gray-700">Apps</a>
+                    </div>
+
+                    <div
+                        class="border border-gray-200 rounded-xl items-center md:col-span-6 flex space-x-3 px-2 py-2 shadow-md">
+                        <img src="../../public/icons/sushi_411029.png" alt="minero icon" class="w-16">
+                        <a href="#" class="text-xl font-semibold  text-gray-700">Themes</a>
+                    </div>
+
+                    <!-- Tarjetas de abajo -->
+                    <div
+                        class="border border-gray-200 rounded-xl items-center md:col-span-6 flex space-x-3 px-2 py-2 shadow-md">
+                        <img src="../../public/icons/umbrella_365856.png" alt="minero icon" class="w-16">
+                        <a href="#" class="text-xl font-semibold  text-gray-700">Fast Connection</a>
+                    </div>
+
+                    <div
+                        class="border border-gray-200 rounded-xl items-center md:col-span-6 flex space-x-3 px-2 py-2 shadow-md">
+                        <img src="../../public/icons/life_7960398.png" alt="minero icon" class="w-16">
+                        <a href="#" class="text-xl font-semibold  text-gray-700">Security</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="col-span-6 h-screen">
+            <img src="../../public/wallpaper.png" alt="Habbo castle">
+        </section>
+    </main>
+</template>
