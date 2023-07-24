9bbbd85915d4bedf49826ffbbdab9a38cfc8da5c
DeveroCode <charli.rey.misterio@gmail.com>
2023-07-23 21:09:54 -0600

    Update: Launch finished

---
+++ src/components/Launch.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="mt-32 bg-blue-100 py-16 rounded-xl mx-16">
+        <div class="mx-16 grid grid-cols-12 gap-10">
+            <div class="col-span-8 pr-32 pl-16">
+                <h3 class="text-5xl font-semibold text-gray-800">Launch your app or theme to millions</h3>
+
+                <h4 class="text-gray-800 font-semibold text-xl mt-5">App Store</h4>
+                <p class="mt-2 text-gray-600">
+                    Get your app in front of the right merchants. With personalized recommendations and multiple biling
+                    options,
+                    the
+                    Shopify App Store is where merchants go to find the apps their business needs most.
+                </p>
+
+                <h4 class="text-gray-800 font-semibold text-xl mt-5">Theme Store</h4>
+                <p class="mt-2 text-gray-600">
+                    Sell your theme to merchants building their brands on the Shopify Theme Store, wich offers flexibe
+                    pricing
+                    and
+                    targeted merchandising.
+                </p>
+            </div>
+            <div class="col-span-4 text-box bg-white border border-black">
+                <div class="mt-16">
+                    <div class="flex px-6 mb-10">
+                        <img src="../../public/icons/coin.png" alt="money icon" class="w-16">
+                        <div class="flex flex-col">
+                            <p class="text-gray-800 font-semibold">$12.38USD</p>
+                            <span class="text-xs text-gray-700">
+                                Revenue generated fromthe Shopify ecosystem (2023)
+                            </span>
+                        </div>
+                    </div>
+
+
+                    <div class="flex px-6 mb-10 items-center">
+                        <img src="../../public/icons/life_7960398.png" alt="money icon" class="w-16">
+                        <p class="text-gray-800 font-semibold">3 in 4 merchants</p>
+                    </div>
+
+                    <div class="flex px-6 mb-10 items-center">
+                        <img src="../../public/icons/peak.png" alt="money icon" class="w-16">
+                        <p class="text-gray-800 font-semibold">Average number of apps installed per merchant</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.text-box {
+    box-shadow: 16px 9px 0px 0px rgba(0, 0, 0), 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+
+}
+</style>
