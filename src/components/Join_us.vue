fd82ab1b8b7f5c7836e26f24e56b04fa0e1267ff
DeveroCode <charli.rey.misterio@gmail.com>
2023-07-27 10:05:03 -0600

    Update:Join Us finished

---
+++ src/components/Join_us.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="mt-32 mb-32 grid grid-cols-12 gap-10">
+        <div class="col-span-3 bg-gray-100 py-5 rounded shadow">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/arroba.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">Join a community of devs</a>
+            </div>
+        </div>
+
+        <div class="col-span-3 bg-gray-100 py-5 rounded shadow border border-gray-200">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/linkin.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">Developer Forums</a>
+                <span class="text-gray-500">Q&A and advice from Shopify community experts</span>
+            </div>
+        </div>
+
+        <div class="col-span-3 bg-gray-100 py-5 rounded shadow border border-gray-200">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/market.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">#Developer in comunity</a>
+                <span class="text-gray-500">Colaboration and connection with Shopify employees</span>
+            </div>
+        </div>
+
+        <div class="col-span-3 bg-gray-100 py-5 rounded shadow border border-gray-200">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/arroba.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">@ShopifyDevs on twitch</a>
+                <span class="text-gray-500">Product announcements, API updates, tips and more</span>
+            </div>
+        </div>
+
+        <div class="col-span-6 bg-gray-100 py-5 rounded shadow border border-gray-200">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/message.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">Developer Changelog</a>
+                <span class="text-gray-500">The latst Shopify ecosystem updates</span>
+            </div>
+        </div>
+
+        <div class="col-span-6 bg-gray-100 py-5 rounded shadow border border-gray-200">
+            <div class="flex items-left ml-5 flex-col">
+                <img src="../../public/icons/microphone.png" alt="join icon" class="w-16">
+                <a href="#" class="font-semibold text-gray-700 text-2xl pr-10 mt-2">ShopifyDev YouTube channel</a>
+                <span class="text-gray-500">Tips and insights in ev design</span>
+            </div>
+        </div>
+    </div>
+</template>
