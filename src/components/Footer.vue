e5686b5ce7d8994f24287e6826ca4b2e734e22ce
DeveroCode <charli.rey.misterio@gmail.com>
2023-07-27 11:10:40 -0600

    Update: footer and settings finished

---
+++ src/components/Footer.vue
@@ -0,0 +1,70 @@
+<script setup>
+</script>
+
+<template>
+    <div class="mt-32 bg-gray-100 py-16 rounded-xl mx-16">
+        <div class="grid grid-col-12">
+            <div class="mx-16 col-span-6 flex justify-between">
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">News and updates</p>
+                    <a href="#" class="text-gray-600">Developer changelog</a>
+                    <a href="#" class="text-gray-600">Shopify Status</a>
+                </nav>
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Blogs</p>
+                    <a href="#" class="text-gray-600">Partner Blog</a>
+                    <a href="#" class="text-gray-600">Engineering Blog</a>
+                    <a href="#" class="text-gray-600">Ux Blog</a>
+                </nav>
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Comunity</p>
+                    <a href="#" class="text-gray-600">Developer Forums</a>
+                    <a href="#" class="text-gray-600">Partner Slack</a>
+                    <a href="#" class="text-gray-600">Facebook Group</a>
+                </nav>
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Comunity</p>
+                    <a href="#" class="text-gray-600">Developer Forums</a>
+                    <a href="#" class="text-gray-600">Partner Slack</a>
+                    <a href="#" class="text-gray-600">Facebook Group</a>
+                </nav>
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Social</p>
+                    <a href="#" class="text-gray-600">
+                        <i class="fa-brands fa-twitch mr-2"></i>
+                        Twitch
+                    </a>
+                    <a href="#" class="text-gray-600">
+                        <i class="fa-brands fa-facebook mr-2"></i>
+                        Facebook
+                    </a>
+                    <a href="#" class="text-gray-600">
+                        <i class="fa-brands fa-youtube mr-2"></i>
+                        YouTube
+                    </a>
+                </nav>
+            </div>
+            <div class="col-span-6 flex flex-row mx-16 space-x-10 mt-10">
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Events</p>
+                    <a href="#" class="text-gray-600">Uniti</a>
+                    <a href="#" class="text-gray-600">Meetups</a>
+                    <a href="#" class="text-gray-600">Partner Town Hall</a>
+                </nav>
+
+
+                <nav class="flex flex-col">
+                    <p class="font-semibold text-gray-700 mb-4">Legal</p>
+                    <a href="#" class="text-gray-600">Terms of Services</a>
+                    <a href="#" class="text-gray-600">API Terms of Services</a>
+                    <a href="#" class="text-gray-600">Privacy Policy</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
